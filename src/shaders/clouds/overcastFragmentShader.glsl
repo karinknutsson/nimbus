f820a83f14855e820ca847141220f667c89bb83c
Karin Knutsson <karinmiriam@gmail.com>
2026-02-08 15:05:51 +0100

    slow movement for overcast clouds

--- src/shaders/clouds/overcastFragmentShader.glsl
+++ src/shaders/clouds/overcastFragmentShader.glsl
@@ -23,29 +23,25 @@ vec2 rotateUv(vec2 uv, float angle, vec2 center) {
 void main() {
     vec2 uv = gl_FragCoord.xy / uResolution;
 
-    vec2 speed = vec2(0.02, 0.01);
+    vec2 speed = vec2(-0.002, -0.001);
     vec2 movingUv = uv + speed * uTime;
 
-    movingUv = rotateUv(movingUv, uTime * 0.03, vec2(0.5));
+    movingUv = rotateUv(movingUv, uTime * 0.003, vec2(0.5));
     movingUv = fract(movingUv);
 
-    // float cloud = texture(uTexture, movingUv).r;
-    float cloud = texture(uTexture, uv).r;
-    // cloud = abs((cloud - 0.5) * 2.0);
+    float cloud = texture(uTexture, movingUv).r;
 
-    // Invert
+    // Invert cloud texture
     float invertedCloud = 1.0 - cloud;
 
+    // Combine original and inverted textures
     float combinedCloud = 1.0 - (cloud * invertedCloud);
-    // cloud = clamp(cloud, 0.4, 1.0);
 
-    // cloud = smoothstep(0.2, 1.0, cloud);
-
-    // float opacity = distance(uv, vec2(0.5));
-    // opacity = smoothstep(0.0, 0.5, opacity);
-    // cloud *= opacity;
+    // Opacity for center view
+    float opacity = distance(uv, vec2(0.5)) * 1.5;
+    opacity = smoothstep(0.0, 0.5, opacity);
 
     vec3 color = vec3(0.85, 0.86, 0.87);
 
-    outColor = vec4(color * combinedCloud, 1.0);
+    outColor = vec4(color * combinedCloud, opacity);
 }
